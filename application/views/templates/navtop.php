975b9853add0e01b42b95131e5bb4128f43f909b
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-19 14:42:56 -0300

    Add Links no Lado Esquerdo do Navbar

--- application/views/templates/navtop.php
+++ application/views/templates/navtop.php
@@ -1,14 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-info">
 	<div class="container-fluid">
 		<a class="navbar-brand" href="#">Prancha de Comunicação</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-				</li>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
 				<li class="nav-item">
 					<a class="nav-link" href="<?= base_url() . 'Contato'; ?>">Contato</a>
 				</li>

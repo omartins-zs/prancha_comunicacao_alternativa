9acd39baa36bc1f74050355f10ea88f4d8ff8c0d
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-20 09:05:19 -0300

    Criando View de Login

---
+++ application/views/pages/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+
+	<title><?= $title ?></title>
+</head>
+
+<body>
+
+	<div class="container">
+		<h1 class=" ">Prancha de Comunicação</h1>
+		<div class="">
+			<div class="col-md-6 ">
+				<div class="card ">
+					<div class="card-body">
+						<h4 class="">Login</h4>
+						<form>
+							<div class="">
+								<label for="username">Usuário</label>
+								<input type="text" class="" id="username" placeholder="Digite seu usuário">
+							</div>
+							<div class="">
+								<label for="password">Senha</label>
+								<input type="password" class="" id="password" placeholder="Digite sua senha">
+							</div>
+							<button type="submit" class="">Acessar</button>
+						</form>
+						<div class="">
+							<a href="#">Esqueci minha senha</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</html>

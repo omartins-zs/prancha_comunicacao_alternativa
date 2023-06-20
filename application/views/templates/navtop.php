f5fab0438ce3bd2412b6e45d9c079f3533c6c900
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-20 16:59:51 -0300

    Add Link para Home no Navbar

--- application/views/templates/navtop.php
+++ application/views/templates/navtop.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-info">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="#">Prancha de Comunicação</a>
+		<a class="navbar-brand" href="<?= base_url() . 'Home'; ?>">Prancha de Comunicação</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>

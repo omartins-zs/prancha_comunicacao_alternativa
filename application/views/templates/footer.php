91dd49d314f0241c022dc2c6c710a57025cb1e25
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-20 14:58:18 -0300

    Personalizando o Footer e Ajustando Posição

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,14 +1,9 @@
-<!-- Footer -->
-<footer class="page-footer mt-2">
-
-	<!-- Copyright -->
-	<div class="footer-copyright text-center py-3">© <?= date('Y') ?> Copyright:
-		<a href="https://portfolio-responsivo-website.netlify.app/">Martins Desenvolvimento</a>
+<footer class="footer text-white mt-auto footerPersonalizado">
+	<div class="container text-center">
+		<div class="py-3">&copy; <?= date('Y') ?> Martins Desenvolvimento</div>
 	</div>
-	<!-- Copyright -->
-
 </footer>
-<!-- Footer -->
+
 </body>
 
 </html>

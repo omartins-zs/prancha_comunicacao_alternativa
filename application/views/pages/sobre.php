71fd721a8188e146c9573791f4a864fe28fdf991
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-20 17:03:48 -0300

    Criando View Sobre e Card de Objetivo

---
+++ application/views/pages/sobre.php
@@ -0,0 +1,21 @@
+<div class="container h-100">
+
+	<h1 class="display-4 titlePrincipal text-center"><?= $titlePage ?></h1>
+
+	<div class="row">
+
+		<div class="col-6">
+
+			<div class="card">
+				<div class="card-header">
+					<h2 class="h2 text-center">OBJETIVO</h2>
+				</div>
+				<div class="card-body">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, quo? Accusamus ratione veniam unde voluptatum accusantium nesciunt dolores impedit commodi, vero ipsam asperiores fuga, culpa maiores adipisci porro perspiciatis nihil quas enim nam. Sapiente molestiae provident impedit aliquid ratione ducimus velit rerum neque voluptas quasi consequatur fugiat unde culpa optio magnam repellendus, ipsam, eum itaque adipisci amet soluta molestias doloremque. Ullam nulla culpa suscipit possimus facilis sint nemo velit tenetur minima voluptatum excepturi, voluptate ipsam voluptatibus officia reprehenderit, aliquam labore? </div>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
